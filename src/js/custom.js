e44e549fccabeea3b6ae9b93f2a33d78c7562d11
Nikita <nikita.kruchinkin@gmail.com>
2017-09-20 14:07:50 +0300

    Some small changes and initial forms functionality

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,4 +1,8 @@
 $(function(){
+
+  $('.form__field').change(function(){
+    $(this).removeClass('error');
+  });
   $('.faq__item').click(function(e){
     $(this).toggleClass('faq__item--opened');
   });
@@ -10,7 +14,7 @@ $(function(){
 
   $('#callback_btn').click(function(e){
     $('.popup__bg').show();
-    $('.popup').show();
+    $('.popup--callback').show();
     return false;
   });
 
@@ -32,5 +36,56 @@ $(function(){
   $(".menu__toggler").click(function(e) {
     $('.menu').toggleClass('active');
   });
+
+  $(".plan__button").click(function(e) {
+    var plan = $(this).data('plan');
+    console.log(plan);
+    $('.popup__bg').show();
+    $('.popup--plan').show();
+    
+    $('input[name="plan"]').val(plan);
+    return false;
+  });
+
+
+  $('.form__button').click(function(e){
+    var target =  $(this).data('target');
+    if (target) {
+      var form = $('#'+target);
+      var method = form.find('input[name="method"]').val();  
+
+      form.find('input[required]').each(function(){
+        var val=$(this).val();
+        if (val=='') {
+          $(this).addClass('error');  
+        }
+      });
+
+      if (form.find('input.error').length) return false;
+      
+      switch(method) {
+        case 'enroll' :
+          enroll(form);
+        break;
+        case 'callback' :
+          callback(form);
+        break;
+      }
+    }
+
+    return false;
+  }); 
+
+
+  
+  function enroll(form) {
+    console.log('enroll function calls');
+
+   
+  }
+  
+  function callback(form) {
+    console.log('callback function calls');
+  }
 
 });
